f987a15a1a1429d87d4ea35f208bb62f20a3feb0
DanShiel27 <danieljshiel@gmail.com>
2025-10-02 19:27:19 -0400

    conditional css selector

--- StorageAndOrganizationTool.UI/StorageAndOrganizationTool.UI/src/app/Features/Rooms/room-form/room-form.component.ts
+++ StorageAndOrganizationTool.UI/StorageAndOrganizationTool.UI/src/app/Features/Rooms/room-form/room-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RoomService } from '../Services/roomservice.service';
 import { RoomDto } from '../../../Core/Models';
@@ -7,7 +7,7 @@ import { skip, tap } from 'rxjs';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { MatRadioModule } from '@angular/material/radio';
+import { MatRadioChange, MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
@@ -18,16 +18,21 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './room-form.component.scss'
 })
 export class RoomFormComponent {
+
   private formBuilder = inject(FormBuilder);
   private roomService = inject(RoomService);
   private destroyRef = inject(DestroyRef)
 
   pictureList: number[] = [1, 2, 3]//TODO: going to move this somewhere else soon, Models.ts maybe?
 
+  picture1Selected = signal('selected');
+  picture2Selected = signal('');
+  picture3Selected = signal('');
+
   roomForm = this.formBuilder.group({
     name: ['', Validators.required],
     description: ['', Validators.required],
-    pictureNumber: [0]
+    pictureNumber: [1]
   })
 
   isAdded$ = this.roomService.getIsAddedObservable().pipe(
@@ -48,12 +53,31 @@ export class RoomFormComponent {
       id: 0,
       name: this.roomForm.value.name!,
       description: this.roomForm.value.description!,
-      pictureNumber: this.roomForm.value.pictureNumber ?? 0
+      pictureNumber: this.roomForm.value.pictureNumber ?? 1
     }
 
     console.log(room)
     //this.roomService.addRoom(room);
     return true;
+  }
+
+  radioChanged($event: MatRadioChange) {
+    //https://v17.angular.io/guide/class-binding
+    if($event.value === 1){
+      this.picture1Selected.set('selected');
+      this.picture2Selected.set('');
+      this.picture3Selected.set('');
+    }
+    else if($event.value === 2){
+      this.picture1Selected.set('');
+      this.picture2Selected.set('selected');
+      this.picture3Selected.set('');
+    }
+    else if($event.value === 3){
+      this.picture1Selected.set('');
+      this.picture2Selected.set('');
+      this.picture3Selected.set('selected');
+    }
   }
 
   handleSubmit(result: boolean){
